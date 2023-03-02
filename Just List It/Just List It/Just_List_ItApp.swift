3c0132017bbc3e9cc2de87a0bac34277f98f890a
Fomagran <fomagran6@gmail.com>
2023-03-01 19:51:18 -0600

    Initial commit

---
+++ Just List It/Just List It/Just_List_ItApp.swift
@@ -0,0 +1,17 @@
+//
+//  Just_List_ItApp.swift
+//  Just List It
+//
+//  Created by Fomagran on 2023/03/01.
+//
+
+import SwiftUI
+
+@main
+struct Just_List_ItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Just List It/Just List ItUITests/Just_List_ItUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Just_List_ItUITestsLaunchTests.swift
+//  Just List ItUITests
+//
+//  Created by Fomagran on 2023/03/01.
+//
+
+import XCTest
+
+final class Just_List_ItUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
